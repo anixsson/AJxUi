8b5237c4863564efac6ace81b7d2ab4c406f6ee1
ANIX <anixpix@gmail.com>
2019-08-30 19:51:51 +0200

    Copy code in

--- js/ajxf.js
+++ js/ajxf.js
@@ -18,14 +18,14 @@
 // }
 
 
-function ajxAccordion (self){
+function ajxAccordion(self) {
 	self.classList.toggle("active");
-		let panel = self.nextElementSibling;
-		if (panel.style.maxHeight){
-			panel.style.maxHeight = null;
-		} else {
-			panel.style.maxHeight = panel.scrollHeight + "px";
-		}
+	let panel = self.nextElementSibling;
+	if (panel.style.maxHeight) {
+		panel.style.maxHeight = null;
+	} else {
+		panel.style.maxHeight = panel.scrollHeight + "px";
+	}
 }
 
 
@@ -35,11 +35,12 @@ function ajxAccordion (self){
 let toggler = document.getElementsByClassName("treeViewBox");
 
 for (let i = 0; i < toggler.length; i++) {
-	toggler[i].addEventListener("dblclick", function() {
+	toggler[i].addEventListener("dblclick", function () {
 		this.parentElement.querySelector(".treeViewNested").classList.toggle("active");
 		this.classList.toggle("check-box");
 	});
 }
+
 
 
 // Get an empty DIV id of a TreeView
@@ -47,31 +48,16 @@ for (let i = 0; i < toggler.length; i++) {
 // Set this DIV to the "AJxTreeView" class
 
 
-/**
- *  Code color
- */
-function ajxCodeColor (self, type){
+function copyText(ID) {
+	var copyText = document.getElementById(ID);
+	console.log(copyText);
+	copyText.style.display = "block";
+	copyText.focus();
+	copyText.select();
+	copyText.setSelectionRange(0, 99999)
+	document.execCommand("copy");
+	copyText.style.display = "none";
+  }
 
-	let colors = {1:"blue", 2:"red"}
 
-	let dictionary;
-	switch (type) {
-		case "javascript":
-			dictionary = {"function":1, "let":2}
-			break;
 
-		default:
-			break;
-	}
-	
-	let dictionary_array = new Array();
-	for (let i = 0; i < Object.keys(dictionary).length; i++) {
-		dictionary_array[i] = Object.keys(dictionary)[i];
-	}
-
-	console.log(dictionary);
-	console.log(dictionary_array);
-
-	
-
-}
